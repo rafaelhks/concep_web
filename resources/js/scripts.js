8db2387bbb509f8a632a0820513c4e5a11b77d1e
Rafael <rafaelfrancisco_97@hotmail.com>
2020-04-10 01:08:16 -0300

    Optimized performance

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,14 +1,16 @@
-var input_cep = document.getElementById('input_cep');
-var cep_card = document.getElementById('cep-card');
-var message = document.getElementById('message');
-var cep_field = document.getElementById('cep');
-var local = document.getElementById('local');
-var bairro = document.getElementById('bairro');
-var logradouro = document.getElementById('logradouro');
-var complemento = document.getElementById('complemento');
-var cod_ibge = document.getElementById('cod-ibge');
-var cod_gia = document.getElementById('cod-gia');
-var loader = document.getElementById('loader');
+const input_cep = document.getElementById('input_cep');
+const cep_card = document.getElementById('cep-card');
+const message = document.getElementById('message');
+const cep_field = document.getElementById('cep');
+const local = document.getElementById('local');
+const bairro = document.getElementById('bairro');
+const logradouro = document.getElementById('logradouro');
+const complemento = document.getElementById('complemento');
+const cod_ibge = document.getElementById('cod-ibge');
+const cod_gia = document.getElementById('cod-gia');
+const loader = document.getElementById('loader');
+const metaType = [{type:'text/plain', ext:'txt'}, {type:'application/json', ext:'json'}];
+
 var current_cep = {};
 
 input_cep.addEventListener("keyup", function(event) {
@@ -26,7 +28,7 @@ function toast(message, time) {
   document.body.appendChild(toast);
 
   setTimeout(function(){ 
-	document.body.removeChild(toast); 
+	  document.body.removeChild(toast); 
   }, time != null ? time : 3000);
 } 
 
@@ -38,7 +40,7 @@ function snackbar(message, time) {
   document.body.appendChild(snackbar);	
 
   setTimeout(function(){ 
-	document.body.removeChild(snackbar); 
+	  document.body.removeChild(snackbar); 
   }, time != null ? time : 3000);
 } 
 
@@ -50,6 +52,7 @@ function remove(id) {
 function clearCEP(){
   cep_card.style.visibility = 'hidden';
   message.style.display = 'block';
+  message.style.visibility = 'visible';
 }
 
 function validate(cep){
@@ -61,14 +64,13 @@ function busca(){
 
   if(!validate(cep)){
     toast('CEP inválido!');
-    return null;
+    return;
   }
 
   message.style.visibility = 'hidden';
   loader.style.display = 'inline-block';
 
   var url = 'https://viacep.com.br/ws/'+cep+'/json/';
-  console.log('fetching: '+url);
 	
   const controller = new AbortController();
   const config = {signal: controller.signal }
@@ -98,13 +100,8 @@ function busca(){
     cep_card.style.visibility = 'visible';
     message.style.display = 'none';
     current_cep = data;
-    cep_field.innerHTML = data.cep;
-    local.innerHTML = 'Local: '+formatData(data.localidade)+', '+formatData(data.uf);
-    bairro.innerHTML =  'Bairro: '+formatData(data.bairro);
-    logradouro.innerHTML = 'Logradouro: '+formatData(data.logradouro);
-    complemento.innerHTML = 'Complemento: '+formatData(data.complemento);
-    cod_ibge.innerHTML = 'Código IBGE: '+formatData(data.ibge);
-    cod_gia.innerHTML = 'Código GIA: '+formatData(data.dia);
+    
+    fillCepCard();
   })
   .catch(err => {
     if (err.name === 'AbortError') {
@@ -112,28 +109,36 @@ function busca(){
     }else{
       toast(err.message);    
     }
+    loader.style.display = 'none';
     message.style.visibility = 'visible';
   })
+}
+
+function fillCepCard(){
+  cep_field.innerHTML = current_cep.cep;
+  local.innerHTML = formatData(current_cep.localidade)+', '+formatData(current_cep.uf);
+  bairro.innerHTML =  formatData(current_cep.bairro);
+  logradouro.innerHTML = formatData(current_cep.logradouro);
+  complemento.innerHTML = formatData(current_cep.complemento);
+  cod_ibge.innerHTML = formatData(current_cep.ibge);
+  cod_gia.innerHTML = formatData(current_cep.gia);
 }
 
 function formatData(data) {
   return data ? data : 'Informação ausente.';
 }
 
-const metaType = [{type:'text/plain', ext:'txt'}, {type:'application/json', ext:'json'}];
-
-
 function downloadFile(type) {
   var dtype = metaType[type];
   var text = '';
   if(type===0) {
-    text = 'CEP: '+cep_field.innerHTML+'\r\n'
-          +local.innerHTML+'\r\n'
-          +bairro.innerHTML+'\r\n'
-          +logradouro.innerHTML+'\r\n'
-          +complemento.innerHTML+'\r\n'
-          +cod_ibge.innerHTML+'\r\n'
-          +cod_gia.innerHTML;
+    text = 'CEP: '+current_cep.cep+'\r\n'
+          +'Local: '+formatData(current_cep.localidade)+', '+formatData(current_cep.uf)+'\r\n'
+          +'Bairro: '+formatData(current_cep.bairro)+'\r\n'
+          +'Logradouro: '+formatData(current_cep.logradouro)+'\r\n'
+          +'Complemento: '+formatData(current_cep.complemento)+'\r\n'
+          +'Código IBGE: '+formatData(current_cep.ibge)+'\r\n'
+          +'Código GIA: '+formatData(current_cep.gia);
   } else if (type===1) {
     text = JSON.stringify(current_cep);	  
   }  
